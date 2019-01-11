fcc87caefa51f47e5614a735b6b90fc81489d947
Thomas <thomas.portet@gmail.com>
2019-01-10 16:47:39 -0800

    3.1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -49,5 +49,15 @@ namespace GradeBook.GradeBooks
 
             return res;
         }
+
+        public override void CalculateStatistics()
+        {
+            int numberOfStudents = Students.Count;
+            if(numberOfStudents < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+        }
     }
 }
