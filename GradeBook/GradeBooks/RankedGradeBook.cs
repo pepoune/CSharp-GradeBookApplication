c515315fff08645c001fbf383d79bd3572637b6c
Thomas <thomas.portet@gmail.com>
2019-01-10 16:41:29 -0800

    2.end

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,10 +13,40 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             char res = 'F';
-            if(Students.Count < 5)
+            int numberOfStudents = Students.Count;
+            if (numberOfStudents < 5)
             {
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
             }
+
+            int numberOfStudentsAbove = 0;
+            foreach(var Student in Students)
+            {
+                if(Student.AverageGrade > averageGrade)
+                {
+                    numberOfStudentsAbove++;
+                }
+            }
+
+            double percentageOfStudentsAbove = 100.0 * numberOfStudentsAbove / numberOfStudents;
+
+            if(percentageOfStudentsAbove < 20)
+            {
+                res = 'A';
+            }
+            else if(percentageOfStudentsAbove < 40)
+            {
+                res = 'B';
+            }
+            else if(percentageOfStudentsAbove < 60)
+            {
+                res = 'C';
+            }
+            else if(percentageOfStudentsAbove < 80)
+            {
+                res = 'D';
+            }
+
             return res;
         }
     }
